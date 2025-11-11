f2aa714080328ebe04dcdbf6cb25de1b442d9efd
Jimmy0016 <jrn.espinoza@yavirac.edu.ec>
2025-11-11 15:16:11 -0500

    two

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hola Liz"})
+    return jsonify({"message": "Hola mundo"})
 
 @app.route('/suma/<int:a>/<int:b>')
 def suma(a, b):
